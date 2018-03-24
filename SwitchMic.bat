5a911f8b3f784db6e5e7afccfd052faa12973c1a
Xoma163 <Xoma163rus@gmail.com>
2018-03-24 14:32:31 +0400

    Изменение мелких недочётов

--- SwitchMic.bat
+++ SwitchMic.bat
@@ -1,12 +1,12 @@
-IF exist ChangeMic.log goto loop1
+IF exist SwitchMic.log goto loop1
 
-echo 0>ChangeMic.log
+echo 0>SwitchMic.log
 nircmdc mutesysvolume 1 default_record
 PlaySound Off.wav
 goto exit
 
 :loop1
-del ChangeMic.log
+del SwitchMic.log
 nircmdc mutesysvolume 0 default_record
 PlaySound On.wav
 :exit
